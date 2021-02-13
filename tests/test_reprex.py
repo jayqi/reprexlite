c9fa5a6e08ef0efaa1accd0043fba8ff2d2238c0
Jay Qi <jayqi@users.noreply.github.com>
2021-02-13 10:32:45 -0800

    Test

--- tests/test_reprex.py
+++ tests/test_reprex.py
@@ -1,14 +1,33 @@
+from collections import namedtuple
 from textwrap import dedent
-from reprexlite import reprex
+from reprexlite.reprex import Reprex
+
+import pytest
 
 
-def test_source():
-    source = dedent(
+TestCase = namedtuple("TestCase", ["input", "expected"])
+
+test_cases = [
+    TestCase(
         """\
-        x = 2+2
-        y = [1,2,3]
-        [i+x for i in y]
-        """
+        arr = [1, 2, 3, 4, 5]
+        [x + 1 for x in arr]
+        """,
+        """\
+        arr = [1, 2, 3, 4, 5]
+        [x + 1 for x in arr]
+        #> [2, 3, 4, 5, 6]
+        """,
     )
+]
 
-    reprex(source)
+
+@pytest.mark.parametrize("test_case", test_cases)
+def test_source(test_case):
+    reprex = Reprex(dedent(test_case.input))
+    print("---")
+    print(str(reprex))
+    print("---")
+    print(dedent(test_case.expected))
+    print("---")
+    assert str(reprex) == dedent(test_case.expected).strip()
