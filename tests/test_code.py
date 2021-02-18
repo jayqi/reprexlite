f51481ad2398a694ec8e750bced8f08c9b6d9a01
Jay Qi <jayqi@users.noreply.github.com>
2021-02-18 01:21:48 -0800

    Fix line number in test

--- tests/test_code.py
+++ tests/test_code.py
@@ -149,7 +149,7 @@ cases = [
 
         sqrt("four")
         #> Traceback (most recent call last):
-        #>   File "{REPO_ROOT / "reprexlite" / "code.py"}", line 70, in evaluate
+        #>   File "{REPO_ROOT / "reprexlite" / "code.py"}", line 69, in evaluate
         #>     result = eval(str(self).strip(), scope, scope)
         #>   File "<string>", line 1, in <module>
         #>   File "<string>", line 2, in sqrt
